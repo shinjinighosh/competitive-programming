2d7a24190e46c23f4da1ef0b2a4f6568d484f28b
Shinjini Ghosh <shinghos@mit.edu>
2020-05-17 18:35:14 -0400

    removing assertion

--- DSA_Learning_Series/FCTRL.py
+++ DSA_Learning_Series/FCTRL.py
@@ -9,14 +9,14 @@ for testcase in range(T):
     zeros_count = 0
     # so, there can be max k^th power of 5 in all the numbers up to N, i.e., in
     # the factorial of N
-    for i in range(1,k+1):
+    for i in range(1, k + 1):
         if len(powers_of_5) > i:
-            zeros_count += N//powers_of_5[i]
+            zeros_count += N // powers_of_5[i]
         else:
             init = powers_of_5[-1]
             old_length = len(powers_of_5)
-            for power in range(old_length, i+2):
-                powers_of_5.append(powers_of_5[-1]*5)
+            for power in range(old_length, i + 2):
+                powers_of_5.append(powers_of_5[-1] * 5)
             # assert len(powers_of_5) >= i
-            zeros_count += N//powers_of_5[i]
+            zeros_count += N // powers_of_5[i]
     print(zeros_count)
