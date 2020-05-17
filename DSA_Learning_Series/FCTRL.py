b5a28caba9c4066b0533f885eb463b7a4c47bc2f
Shinjini Ghosh <shinghos@mit.edu>
2020-05-17 18:31:02 -0400

    optimizing by memoization

--- DSA_Learning_Series/FCTRL.py
+++ DSA_Learning_Series/FCTRL.py
@@ -1,6 +1,7 @@
 from math import log
 
 T = int(input())
+powers_of_5 = [1]
 
 for testcase in range(T):
     N = int(input())
@@ -9,5 +10,13 @@ for testcase in range(T):
     # so, there can be max k^th power of 5 in all the numbers up to N, i.e., in
     # the factorial of N
     for i in range(1,k+1):
-        zeros_count += N//(5**i)
+        if len(powers_of_5) > i:
+            zeros_count += N//powers_of_5[i]
+        else:
+            init = powers_of_5[-1]
+            old_length = len(powers_of_5)
+            for power in range(old_length, i+2):
+                powers_of_5.append(powers_of_5[-1]*5)
+            # assert len(powers_of_5) >= i
+            zeros_count += N//powers_of_5[i]
     print(zeros_count)
