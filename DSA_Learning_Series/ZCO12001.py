3c2d73895841a33246d6c6b6dbeadeb3e6cf4c05
Shinjini Ghosh <shinghos@mit.edu>
2020-05-25 15:49:34 -0400

    finished maximum bracket subsequence

--- DSA_Learning_Series/ZCO12001.py
+++ DSA_Learning_Series/ZCO12001.py
@@ -17,4 +17,17 @@ for element in bracket_expression:
 max_depth = max(nesting_depths)
 first_deep_nested = nesting_depths.index(max_depth) + 1 # because 1-indexed
 
-print(f"{max_depth} {first_deep_nested}")
+
+max_subsequence_start = 0
+max_subsequence_length = 0
+
+for idx, nesting in enumerate(nesting_depths):
+    if nesting == 0:
+        continue
+    subsequence_length = nesting_depths.index(nesting-1, idx) - idx + 1
+    if subsequence_length > max_subsequence_length:
+        max_subsequence_length = subsequence_length
+        max_subsequence_start = idx + 1 #because 1-indexed
+
+
+print(f"{max_depth} {first_deep_nested} {max_subsequence_length} {max_subsequence_start}")
