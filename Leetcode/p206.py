6a2bfdd26cccefab00b239f05a797f4c2cf52cf2
Shinjini Ghosh <shinghos@mit.edu>
2020-05-28 20:03:55 -0400

    finished p206

---
+++ Leetcode/p206.py
@@ -0,0 +1,18 @@
+# Reverse Linked List
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+        else:
+            res = self.reverseList(head.next)
+            head.next.next = head
+            head.next = None
+            return res
