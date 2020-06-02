9afe6465bbe08f85579c023d448ea3c05a47b847
Shinjini Ghosh <shinghos@mit.edu>
2020-06-02 16:19:50 -0400

    finished atoi with regex

--- Leetcode/Problems/p8.py
+++ Leetcode/Problems/p8.py
@@ -1,27 +1,15 @@
 # implement atoi
+import re
 
 
 def myAtoi(str: str) -> int:
-    str = str.strip()
-    negative = False
-    if not str or ((str.count("+") + str.count("-")) > 1):
-        print("count more for str", str)
+    # str = str.strip()
+    pattern = "^\s*([-\+]?\d+)"
+    match = re.search(pattern, str)
+    if not match:
         return 0
-    if str[0] == "-":
-        negative = True
-    str = str.strip("+-")
-    stripped_string = ""
-    for char in str:
-        if char in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
-            stripped_string += char
-        else:
-            break
     try:
-        # print(stripped_string)
-        if negative:
-            res = -1 * int(stripped_string)
-        else:
-            res = int(stripped_string)
+        res = int(match.group(1))
         if res > 2**31 - 1:
             return 2**31 - 1
         elif res < -2**31:
