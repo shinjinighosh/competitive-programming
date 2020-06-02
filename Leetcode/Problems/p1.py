57811c4242e3fce6a7b6f7f43a96513cafdd6612
Shinjini Ghosh <shinghos@mit.edu>
2020-06-01 21:07:26 -0400

    finished p1

---
+++ Leetcode/Problems/p1.py
@@ -0,0 +1,9 @@
+# return indices which add up to target
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums) - 1):
+            for j in range(i + 1, len(nums)):
+                if (nums[i] + nums[j]) == target:
+                    return [i, j]
