edea2023b90d54639a278157a400eecea64886b7
Shinjini Ghosh <shinghos@mit.edu>
2020-06-06 23:11:27 -0400

    added documentation

--- Leetcode/Contests/weekly_192_shuffle_array.py
+++ Leetcode/Contests/weekly_192_shuffle_array.py
@@ -1,3 +1,16 @@
+'''
+5428. Shuffle the Array
+User Accepted:6794
+User Tried:6848
+Total Accepted:6976
+Total Submissions:7425
+Difficulty:Easy
+Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+
+Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+'''
+
+
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
         res = []
