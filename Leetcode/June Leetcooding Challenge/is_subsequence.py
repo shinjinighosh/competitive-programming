8284751e910472f831b3caeaab30d34fb6ea6413
Shinjini Ghosh <shinghos@mit.edu>
2020-06-09 18:40:00 -0400

    completed is subseq

---
+++ Leetcode/June Leetcooding Challenge/is_subsequence.py
@@ -0,0 +1,25 @@
+# return if s is subsequence of t
+
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        idx = 0
+        for char in s:
+            if idx >= len(t):
+                return False  # reached end of t
+            while t[idx] != char:
+                idx += 1
+                if idx >= len(t):
+                    return False  # reached end of t
+            if t[idx] == char:
+                idx += 1
+        return True
+
+        # if char not in t[idx:]:
+        #     return False
+        # else:
+        #     if idx > len(t):
+        #         return False
+        #     else:
+        #         idx += t[idx:].index(char)
+        # return True
