b734584be500164e80b3fe0de8db77ef7c38a556
Shinjini Ghosh <shinghos@mit.edu>
2020-06-09 23:33:02 -0400

    added alt heap sol

--- Leetcode/May Leetcoding Challenge/k_closest_points.py
+++ Leetcode/May Leetcoding Challenge/k_closest_points.py
@@ -3,7 +3,22 @@ class Solution:
         def square_of_dist_to_origin(point):
             x = point[0]
             y = point[1]
-            return (x*x + y*y)
-        return sorted(points, key = square_of_dist_to_origin)[:K]
-            
-        
+            return (x * x + y * y)
+        return sorted(points, key=square_of_dist_to_origin)[:K]
+
+        # alternate heap solution, slightly slower than the sol above
+        '''
+        res = []
+        for i in range(len(points)):
+            x, y = points[i]
+            res.append(((x*x+y*y), [x,y]))
+
+        import heapq
+        heapq.heapify(res)
+
+        arr = []
+        for i in range(K):
+            arr.append(heapq.heappop(res)[1])
+
+        return arr
+        '''
