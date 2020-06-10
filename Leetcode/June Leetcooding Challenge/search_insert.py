dc4faa3c749091877380cea75535b16b944dca18
Shinjini Ghosh <shinghos@mit.edu>
2020-06-10 03:14:25 -0400

    finished search insert

---
+++ Leetcode/June Leetcooding Challenge/search_insert.py
@@ -0,0 +1,19 @@
+# Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+# You may assume no duplicates in the array.
+
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        if nums and target <= nums[0]:
+            return 0
+        for i in range(len(nums) - 1):
+            if nums[i] == target:
+                return i
+            if nums[i] < target < nums[i + 1]:
+                return i + 1
+        if nums[len(nums) - 1] == target:
+            return len(nums) - 1
+        if len(nums) == 1 and target > nums[0]:
+            return 1
+        return i + 2
