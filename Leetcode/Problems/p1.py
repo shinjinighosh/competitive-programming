373584d885f2b0f9e09c741ad389fc72f7d2b75e
Shinjini Ghosh <shinghos@mit.edu>
2020-06-10 21:35:51 -0400

    faster one pass hash array sol

--- Leetcode/Problems/p1.py
+++ Leetcode/Problems/p1.py
@@ -3,7 +3,15 @@
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums) - 1):
-            for j in range(i + 1, len(nums)):
-                if (nums[i] + nums[j]) == target:
-                    return [i, j]
+        # for i in range(len(nums) - 1):
+        #     for j in range(i + 1, len(nums)):
+        #         if (nums[i] + nums[j]) == target:
+        #             return [i, j]
+
+        # faster one-pass hash array solution
+        mapping = {}
+        for i in range(len(nums)):
+            if (target - nums[i]) in mapping:
+                return [i, mapping[target - nums[i]]]
+            else:
+                mapping[nums[i]] = i
