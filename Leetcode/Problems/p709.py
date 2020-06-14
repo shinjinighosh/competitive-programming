45b516fb9c7f7c8c51fb8e45658eb91695077f24
Shinjini Ghosh <shinghos@mit.edu>
2020-06-14 01:56:48 -0400

    finished lower case

---
+++ Leetcode/Problems/p709.py
@@ -0,0 +1,11 @@
+# 709. To Lower Case
+
+'''
+Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+
+'''
+
+
+class Solution:
+    def toLowerCase(self, str: str) -> str:
+        return str.lower()
