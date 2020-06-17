5c61b6b66edd9475108b5688509510bf910e845b
Shinjini Ghosh <shinghos@mit.edu>
2020-06-16 20:08:43 -0400

    completed sum of left leaves

--- Leetcode/Problems/p404.py
+++ Leetcode/Problems/p404.py
@@ -14,11 +14,19 @@ Find the sum of all left leaves in a given binary tree.
 
 class Solution:
     def sumOfLeftLeaves(self, root: TreeNode) -> int:
-        if not root or (not root.left and not root.right):
+        if not root:
             return 0
-        elif not root.left and root.right:  # recurse on right side
+        elif not root.left and not root.right:
+            return 0
+        elif root.left and not root.right:
+            if not root.left.left and not root.left.right:
+                return root.left.val
+            else:
+                return self.sumOfLeftLeaves(root.left)
+        elif root.right and not root.left:
             return self.sumOfLeftLeaves(root.right)
-        elif not root.right and root.left:
-            return root.left.val + self.sumOfLeftLeaves(root.left)
-        elif root.left and root.right:  # both sides present
-            return root.left.val + self.sumOfLeftLeaves(root.left) + self.sumOfLeftLeaves(root.right)
+        else:
+            if not root.left.left and not root.left.right:
+                return root.left.val + self.sumOfLeftLeaves(root.right)
+            else:
+                return self.sumOfLeftLeaves(root.left) + self.sumOfLeftLeaves(root.right)
