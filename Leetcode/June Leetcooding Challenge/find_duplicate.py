c48dd3cf2011013da507549e03d0788409d758f2
Shinjini Ghosh <shinghos@mit.edu>
2020-06-25 20:07:47 -0400

    finished in O(nlogn)

---
+++ Leetcode/June Leetcooding Challenge/find_duplicate.py
@@ -0,0 +1,14 @@
+#   Find the Duplicate Number
+
+'''
+Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive), prove that at least one duplicate number must exist. Assume that there is only one duplicate number, find the duplicate one.
+
+'''
+
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                return nums[i]
