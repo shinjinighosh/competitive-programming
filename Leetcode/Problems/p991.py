75e2c56a2fda221f91be0ef2ee5721d1ad9812a7
Shinjini Ghosh <shinghos@mit.edu>
2020-07-12 15:46:08 -0400

    finished with a greedy approach

---
+++ Leetcode/Problems/p991.py
@@ -0,0 +1,24 @@
+# 991. Broken Calculator
+
+'''
+On a broken calculator that has a number showing on its display, we can perform two operations:
+
+Double: Multiply the number on the display by 2, or;
+Decrement: Subtract 1 from the number on the display.
+Initially, the calculator is displaying the number X.
+
+Return the minimum number of operations needed to display the number Y.
+'''
+
+
+class Solution:
+    def brokenCalc(self, X: int, Y: int) -> int:
+        # greedily always diving Y by 2
+        res = 0
+        while Y > X:
+            res += 1
+            if Y % 2 == 1:
+                Y += 1
+            else:
+                Y /= 2
+        return int(res + X - Y)
