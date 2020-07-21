22eebc865db5ecda1f17a5449a2eea0fd1e99a4c
Shinjini Ghosh <shinghos@mit.edu>
2020-07-20 20:39:59 -0400

    finished with caching

---
+++ Leetcode/Problems/p509.py
@@ -0,0 +1,18 @@
+# 509. Fibonacci Number
+
+'''
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+F(0) = 0,   F(1) = 1
+F(N) = F(N - 1) + F(N - 2), for N > 1.
+Given N, calculate F(N).
+'''
+
+
+class Solution:
+    cache = {0: 0, 1: 1}
+
+    def fib(self, N: int) -> int:
+        if N not in Solution.cache:
+            Solution.cache[N] = self.fib(N - 1) + self.fib(N - 2)
+        return Solution.cache[N]
