0477409e27f37fee5aaaed6c335f2f2ba972db2c
Shinjini Ghosh <shinghos@mit.edu>
2020-07-29 12:56:50 -0400

    finished with dp

---
+++ Leetcode/Problems/p198.py
@@ -0,0 +1,21 @@
+# 198. House Robber
+
+'''
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+'''
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        if len(nums) <= 2:
+            return max(nums)
+        dp = [0 for _ in range(len(nums))]
+        dp[0] = nums[0]
+        dp[1] = nums[1]
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i - 2], dp[i - 3]) + nums[i]
+        return max(dp[-1], dp[-2])
