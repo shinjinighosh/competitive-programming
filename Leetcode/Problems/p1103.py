5b5f06e175b804e7e9452dd75f92d6c7fc014d1a
Shinjini Ghosh <shinghos@mit.edu>
2020-08-02 19:24:40 -0400

    finished distribute candies

---
+++ Leetcode/Problems/p1103.py
@@ -0,0 +1,31 @@
+# 1103. Distribute Candies to People
+
+'''
+We distribute some number of candies, to a row of n = num_people people in the following way:
+
+We then give 1 candy to the first person, 2 candies to the second person, and so on until we give n candies to the last person.
+
+Then, we go back to the start of the row, giving n + 1 candies to the first person, n + 2 candies to the second person, and so on until we give 2 * n candies to the last person.
+
+This process repeats (with us giving one more candy each time, and moving to the start of the row after we reach the end) until we run out of candies.  The last person will receive all of our remaining candies (not necessarily one more than the previous gift).
+
+Return an array (of length num_people and sum candies) that represents the final distribution of candies.
+'''
+
+
+class Solution:
+    def distributeCandies(self, candies: int, num_people: int) -> List[int]:
+        curr = 1
+        ptr = 0
+        res = [0 for _ in range(num_people)]
+        while candies >= 0:
+            if candies <= curr:
+                res[ptr] += candies
+                candies = 0
+                break
+            else:
+                res[ptr] += curr
+                candies -= curr
+                curr += 1
+                ptr = (ptr + 1) % num_people
+        return res
