12dfcfb9599359c6dae98c5ea74eba23bb3efd5c
Shinjini Ghosh <shinghos@mit.edu>
2020-08-28 21:06:20 -0400

    finished rand10 using rand7

---
+++ Leetcode/August Leetcoding Challenge/rand_10.py
@@ -0,0 +1,20 @@
+# Implement Rand10() Using Rand7()
+
+'''
+Given a function rand7 which generates a uniform random integer in the range 1 to 7, write a function rand10 which generates a uniform random integer in the range 1 to 10.
+
+Do NOT use system's Math.random().
+'''
+
+# The rand7() API is already defined for you.
+# def rand7():
+# @return a random integer in the range 1 to 7
+
+
+class Solution:
+    def rand10(self):
+        """
+        :rtype: int
+        """
+        c = (rand7() - 1) * 7 + rand7() - 1
+        return self.rand10() if c >= 40 else (c % 10) + 1
