47175bea3b743f945f2d9aa5092f2e0a47c07c86
Shinjini Ghosh <shinghos@mit.edu>
2024-05-15 18:33:56 -0400

    new sol

---
+++ Leetcode/Problems/p2_3.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        res = ListNode()
+        curr = res
+        carry = 0
+        while l1 or l2:
+            d1 = l1.val if l1 else 0
+            d2 = l2.val if l2 else 0
+            total = d1 + d2 + carry
+            digit = total % 10
+            carry = total // 10
+            curr.next = ListNode(digit)
+            curr = curr.next
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+        if carry:
+            curr.next = ListNode(int(carry))
+
+        return res.next
