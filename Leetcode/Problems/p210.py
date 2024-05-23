ac549dfaf11cb0055a96e2e83c71abf1a15038f7
Shinjini Ghosh <shinghos@mit.edu>
2024-05-23 18:00:11 -0400

    moving

--- Leetcode/Problems/p210.py
+++ Leetcode/Problems/p210.py
@@ -1,12 +1,32 @@
-class Solution:
-    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        # sliding window of size k
-        window = set()
+from collections import defaultdict, deque
 
-        for i in range(len(nums)):
-            if nums[i] in window:
-                return True
-            window.add(nums[i])
-            if len(window) > k:
-                window.remove(nums[i-k])
-        return False
+class Solution:
+    def findOrder(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: List[int]
+        """
+
+        # make graph
+        adj_list = defaultdict(list)
+        indegree = {}
+        for dest, src in prerequisites:
+            adj_list[src].append(dest)
+            indegree[dest] = indegree.get(dest, 0) + 1
+
+        zero_indegree_queue = deque([k for k in range(numCourses) if k not in indegree])
+        topological_sorted_order = []
+
+        while zero_indegree_queue:
+            vertex = zero_indegree_queue.popleft()
+            topological_sorted_order.append(vertex)
+
+            if vertex in adj_list:
+                for neighbor in adj_list[vertex]:
+                    indegree[neighbor] -= 1
+
+                    if indegree[neighbor] == 0:
+                        zero_indegree_queue.append(neighbor)
+
+        return topological_sorted_order if len(topological_sorted_order) == numCourses else []
--- Leetcode/Problems/p392.py
+++ Leetcode/Problems/p392.py
@@ -1,24 +1,25 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        idx = 0
-        for char in s:
-            if idx >= len(t):
-                return False # reached end of t
-            while t[idx] != char:
-                idx += 1
-                if idx >= len(t):
-                    return False # reached end of t
-            if t[idx] == char:
-                idx += 1
-        return True
-               
+        s_ptr = 0
+        t_ptr = 0
+
+        if not s:
+            return True
+        elif not t:
+            return False
+
+        while t:
+            curr_s = s[s_ptr]
+            curr_t = t[t_ptr]
+            if curr_s == curr_t:
+                s_ptr += 1
+                t_ptr += 1
+                if s_ptr == len(s):
+                    return True # have reached the end of s
+            else:
+                t_ptr += 1
             
-            # if char not in t[idx:]:
-            #     return False
-            # else:
-            #     if idx > len(t):
-            #         return False
-            #     else:
-            #         idx += t[idx:].index(char) 
-        # return True
-    
+            if t_ptr >= len(t):
+                return False # exhausted t 
+        
+        return False
