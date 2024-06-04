867c46fa523cd152dfd75224949e4ba05beab009
Shinjini Ghosh <shinghos@mit.edu>
2024-06-04 03:08:45 -0400

    new sol

---
+++ Leetcode/Problems/p117.py
@@ -0,0 +1,26 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        queue = [(root, 0)]
+        while queue:
+            curr, level = queue.pop(0)
+            if not curr:
+                continue
+            for next_node, level_node in queue:
+                if level_node == level:
+                    curr.next = next_node
+                    break
+            if curr.left:
+                queue.append((curr.left, level + 1))
+            if curr.right:
+                queue.append((curr.right, level + 1))
+        return root
