75b300764321533bf0055da4e39016d32306e59d
Shinjini Ghosh <shinghos@mit.edu>
2024-06-10 18:20:33 -0400

    new sol

---
+++ Leetcode/Problems/p559.py
@@ -0,0 +1,16 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if not root:
+            return 0
+        if not root.children:
+            return 1
+        return 1 + max([self.maxDepth(i) for i in root.children])
+        
